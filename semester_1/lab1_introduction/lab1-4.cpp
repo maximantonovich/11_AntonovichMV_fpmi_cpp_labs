0a5a6bc00dcd71b3d5a9b6795f2b7e7a00f5d8bf
maximantonovich <antonovich.maxim@gmail.com>
2025-09-09 10:50:46 +0300

    добавил проверки и изменил язык на англ

--- semester_1/lab1_introduction/lab1-4.cpp
+++ semester_1/lab1_introduction/lab1-4.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
- int main() {
-    setlocale(LC_ALL, "Ru");
+int main() {
     int k, first_odd = 1, first_even = 2, result = 1;
-    cin >> k;
+    std::cout << "write number k";
+    if (!(std::cin >> k)) {
+        std::cout << "error";
+        return 1;
+    }
     if (k >= 0)
     {
-        
+
         if (k % 2 == 0) //если "k" чётное то остаток = 0
         {
-            for (int i = 0; i < k/2; i++)
-        {
-            result *= first_even;
-            first_even += 2;
-        }
-     cout << result;
+            for (int i = 0; i < k / 2; i++)
+            {
+                result *= first_even;
+                first_even += 2;
+            }
+            std::cout << result;
         }
 
 
         else {
-        for (int i = 0; i <= round(k/2); i++)
-        {
-            result *= first_odd;
-            first_odd += 2;
+            for (int i = 0; i <= round(k / 2); i++)
+            {
+                result *= first_odd;
+                first_odd += 2;
+            }
+            std::cout << result;
         }
-    cout << result;
     }
-}
-     else {
-        cout << "Число должно быть не отрицательным";
+    else {
+        std::cout << "number cant be negative";
+        return 1;
     }
 }
