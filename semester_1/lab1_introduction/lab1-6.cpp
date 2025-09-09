e1d5826db55dd79524f867d8580e5af9f6330c3b
maximantonovich <antonovich.maxim@gmail.com>
2025-09-09 10:55:48 +0300

    добавил проверки и изменил язык на англ

--- semester_1/lab1_introduction/lab1-6.cpp
+++ semester_1/lab1_introduction/lab1-6.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
-using namespace std;
- int main() {
-    setlocale(LC_ALL, "Ru");
+int main() {
     int n, first_odd = 1, result = 1;
-    cin >> n;
+    std::cout << "write how many odd numbers?\n";
+    if (!(std::cin >> n)) {
+        std::cout << "error";
+        return 1;
+    }
     if (n > 0)
     {
-    for (int i = 0; i < n; i++)
-    {
-        result *= first_odd;
-        first_odd += 2;
+        for (int i = 0; i < n; i++)
+        {
+            result *= first_odd;
+            first_odd += 2;
+        }
+        std::cout << result;
     }
-    cout << result;
+    else if (n == 0) //частный случай с нулём
+    {
+        std::cout << "0";
+    }
+    else std::cout << "number must be not negative";
 }
-else if (n==0) //частный случай с нулём
-{
-    cout << "0";
-}
-else cout <<"Число n должно быть не отрицательным";
- }
+
