23e9b5744e72eab3f9700b6ea0b823300bc36002
maximantonovich <antonovich.maxim@gmail.com>
2025-09-09 10:45:05 +0300

    добавил проверки и изменил язык на англ

--- semester_1/lab1_introduction/lab1-3.cpp
+++ semester_1/lab1_introduction/lab1-3.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 int main() {
-    setlocale(LC_ALL, "Ru");
     int n, s = 0;
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-    s +=pow(i,i);
+    std::cout << "write number n";
+    if (!(std::cin >> n)) {
+        std::cout << "error";
+        return 1;
     }
-    cout << s;
+    if (n < 1)
+    {
+        std::cout << "n must be greater than 1";
+        return 1;
+    }
+    for (int i = 1; i <= n; i++) {
+        s += pow(i, i);
+    }
+    std::cout << s;
 }
 
 /* после "9" программа показывает минимальное значение int так как все биты уже использованы,
  и программа использует бит знака.*/ 
+
