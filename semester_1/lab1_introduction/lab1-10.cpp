43fc2185395642a51ef33f18d32d8ed7e944144a
maximantonovich <antonovich.maxim@gmail.com>
2025-09-09 10:59:33 +0300

    добавил проверки и изменил язык на англ

--- semester_1/lab1_introduction/lab1-10.cpp
+++ semester_1/lab1_introduction/lab1-10.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
-using namespace std;
-
 int main() {
-  setlocale(LC_ALL, "Ru");
     int num, first, second, third, four, five, six, summ1, summ2;
-    cout << "Введите шестизначное число";
-    cin >> num;
-    if (num >100000 && num <1000000)
-    { 
-        first = num /100000;
-        second = (num /10000) % 10; 
+    std::cout << "write six digit number\n";
+    if (!(std::cin >> num)) {
+        std::cout << "error";
+        return 1;
+    }
+    if (num > 100000 && num < 1000000)
+    {
+        first = num / 100000;
+        second = (num / 10000) % 10;
         third = (num / 1000) % 10;
-        four = (num/100) % 10;
-        five = (num/10) %10;
-        six = num%10;
+        four = (num / 100) % 10;
+        five = (num / 10) % 10;
+        six = num % 10;
         summ1 = first + second + third;
         summ2 = four + five + six;
         if (summ1 == summ2)
         {
-            cout << "Счастливое";
+            std::cout << "lucky";
         }
-        else {cout << "Не счастливое";
+        else {
+            std::cout << "not lucky";
         }
-}
-  else {
-    cout << "Число должно быть шестизначное";
-  }
+    }
+    else {
+        std::cout << "number has to be 6 digits long";
+    }
 }
