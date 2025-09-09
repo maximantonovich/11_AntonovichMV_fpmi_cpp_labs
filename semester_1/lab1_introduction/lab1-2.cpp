aca6053d0461b9fa62c3612cd5ab1ab084403b6c
maximantonovich <antonovich.maxim@gmail.com>
2025-09-09 10:38:09 +0300

    изменил рус на англ, добавил проверку

--- semester_1/lab1_introduction/lab1-2.cpp
+++ semester_1/lab1_introduction/lab1-2.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
- int main() {
+int main() {
     setlocale(LC_ALL, "Ru");
     int n, even;
     int odd = 1, a = 1;
-    cout << "Введите число n" << endl;
-    cin >> n;
-    if (n < 1)  {
-        cout << "n должно быть больше 1" << endl;
+    std::cout << "write number n\n";
+    if (!(std::cin >> n))
+    {
+        std::cout << "error";
         return 1;
     }
-    even = n / 2 * (4 + 2 * (n/2 - 1)) / 2; //формула суммы арифметической прогресии
-    
-    for (int i = 0; i < round(n/2); i++)
+    if (n < 1) {
+        std::cout << "n must be greater than 1\n";
+        return 1;
+    }
+    even = n / 2 * (4 + 2 * (n / 2 - 1)) / 2; //формула суммы арифметической прогресии
+
+    for (int i = 0; i < round(n / 2); i++)
     {
         odd *= a;
         a += 2;
     }
-    cout << "сумма чётных чисел: "<< even  << "\n" << "произведение нечётных чисел: " << odd;
- }
+    std::cout << "summ of even: " << even << "\n" << "multiplication of odd: " << odd << "\n";
+
+}
