af34f9f363f9b3053f3490143833c9446f0b1313
maximantonovich <antonovich.maxim@gmail.com>
2025-09-22 18:09:13 +0300

    добавил проверки

--- semester_1/lab2_integer_arithmetics/lab2-2.cpp
+++ semester_1/lab2_integer_arithmetics/lab2-2.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 #include <cmath>
 int main() {
-  int n, n_save, count = 0, num, temp, result = 0, digit_place = 1;
-  std::cin >> n;
-  n_save = n;
+  int n, n_save, count = 0, digit, temp, result = 0, digit_place = 1;
+  std::cout << "write number n\n";
+  if (!(std::cin >> n))
+  {
+    std::cout << "n must be a number\n";
+    return 0;
+  }
+  if (n <=0)
+  {
+    std::cout << "n must be greater than 0";
+    return 0;
+  }
+     
+  n_save = n; 
   while (n !=0)
   {
-    num = n % 10;
+    digit = n % 10;
     n /= 10;
     temp = n_save;
     while (temp != 0 )
     {
-      if (num == temp % 10)
+      if (digit == temp % 10)
       {
         count++;
       }
       temp /= 10;
       
     }
-    if (count % 2 != 0)
+    if (count % 2 != 0) 
     {
-      result += digit_place * num;
-      digit_place *= 10;
+      result += digit_place * digit;
+      digit_place *= 10; 
     }
     count = 0;
 
   }
-  std::cout << result;
+  std::cout << "the result is: " << result;
 }
