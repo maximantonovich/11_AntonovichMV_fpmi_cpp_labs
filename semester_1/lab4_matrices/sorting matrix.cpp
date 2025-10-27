d6c75324104a8496b1f50200976843a8943fe04e
maximantonovich <antonovich.maxim@gmail.com>
2025-10-27 18:47:30 +0300

    added new sorting algorithms, fixed bugs

--- semester_1/lab4_matrices/sorting matrix.cpp
+++ semester_1/lab4_matrices/sorting matrix.cpp
@@ -1,12 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 #include <random>
 void mas(int**& arr, int m, int n); // инициализация массива
 void keyboard_input(int** arr1, int m, int n); // ввод с клавиатуры
 void random_input(int** arr2, int m, int n); // рандомный ввод
-void bubble_sort(int** arr, int m, int n, bool (*comp)(int, int));
+
+
 bool descend_comp(int a, int b);
 bool ascend_comp(int a, int b);
+
+void bubble_sort(int** arr, int m, int n, bool (*comp)(int, int));
 void count_sort(int** arr, int m, int n, bool(*comp)(int, int));
+void insert_sort(int** arr, int m, int n, bool(*comp)(int, int));
+void shell_sort(int** arr, int m, int n, bool(*comp)(int, int));
+
+void tmp(int**& temp, int m, int n);
+void merge(int **arr, int m, int left, int mid, int right, int** temp, bool(*comp)(int, int));
+void mergeSort(int** arr,int m, int left, int right, int** temp, bool(*comp)(int, int));
+
+
 int main()
 {
     int m, n;
@@ -48,40 +59,97 @@ int main()
 
     for (int i = 0; i < m; i++)
     {
-        std::cout << "write what kind of sorting you want for line: " << i + 1 << " 1-bubble sort, 2-count sort, 3-insert sort, \n";
+        std::cout << "write what kind of sorting you want for line: " << i + 1 << " 1-bubble sort, 2-count sort, 3-insert sort, 4-shell sort, 5-merge sort. \n";
         int sort_method;
         std::cin >> sort_method;
         switch (sort_method)
         {
-        case 1:
-        {
-            std::cout << "do you want to sort in descending order? or in ascending order? \n" << "write 1-ascending, 2-descending\n";
-            int sort_type;
-            std::cin >> sort_type;
-            switch (sort_type)
+            case 1:
             {
-            case 1: bubble_sort(arr, i, n, ascend_comp); break;
-            case 2: bubble_sort(arr, i, n, descend_comp); break;
-            default: std::cout << "pls write either 1 or 2"; return 1;
+                std::cout << "do you want to sort in descending order? or in ascending order? \n" << "write 1-ascending, 2-descending\n";
+                int sort_type;
+                std::cin >> sort_type;
+                switch (sort_type)
+                {
+                case 1: bubble_sort(arr, i, n, ascend_comp); break;
+                case 2: bubble_sort(arr, i, n, descend_comp); break;
+                default: std::cout << "pls write either 1 or 2"; return 1;
+                }
+                break;
             }
-        }
-        case 2:
-        {
-            std::cout << "do you want to sort in descending order? or in ascending order? \n" << "write 1-ascending, 2-descending\n";
-            int sort_type;
-            std::cin >> sort_type;
-            switch (sort_type)
+            case 2:
             {
-            case 1: count_sort(arr, i, n, ascend_comp); break;
-            case 2: count_sort(arr, i, n, descend_comp); break;
-            default: std::cout << "pls write either 1 or 2"; return 1;
+                std::cout << "do you want to sort in descending order? or in ascending order? \n" << "write 1-ascending, 2-descending\n";
+                int sort_type;
+                std::cin >> sort_type;
+                switch (sort_type)
+                {
+                case 1: count_sort(arr, i, n, ascend_comp); break;
+                case 2: count_sort(arr, i, n, descend_comp); break;
+                default: std::cout << "pls write either 1 or 2"; return 1;
+                }
+                break;
             }
-        }
-        default:
-            break;
-
+            case 3:
+            {
+                std::cout << "do you want to sort in descending order? or in ascending order? \n" << "write 1-ascending, 2-descending\n";
+                int sort_type;
+                std::cin >> sort_type;
+                switch (sort_type)
+                {
+                case 1: insert_sort(arr, i, n, ascend_comp); break;
+                case 2: insert_sort(arr, i, n, descend_comp); break;
+                default: std::cout << "pls write either 1 or 2"; return 1;
+                }
+                break;
+            }
+            case 4:
+            {
+                std::cout << "do you want to sort in descending order? or in ascending order? \n" << "write 1-ascending, 2-descending\n";
+                int sort_type;
+                std::cin >> sort_type;
+                switch (sort_type)
+                {
+                case 1: shell_sort(arr, i, n, descend_comp); break;
+                case 2: shell_sort(arr, i, n, ascend_comp); break;
+                default: std::cout << "pls write either 1 or 2"; return 1;
+                }
+                break;
+            }
+            case 5:
+            {  
+                int** temp;
+                tmp(temp, m, n);
+                std::cout << "do you want to sort in descending order? or in ascending order? \n" << "write 1-ascending, 2-descending\n";
+                int sort_type;
+                std::cin >> sort_type;
+                switch (sort_type)
+                {
+                case 1: mergeSort(arr, i, 0, n, temp, descend_comp); break;
+                case 2: mergeSort(arr, i, 0, n, temp, ascend_comp); break;
+                default: std::cout << "pls write either 1 or 2"; return 1;
+                }
+                for (int k = 0; k < n; k++)
+                {
+                    std::cout << arr[i][k] << " ";
+                }
+                    std::cout << "\n";
+                break;
+            }
         }
     }
+
+    std::cout << "Your final matrix is:\n";
+   for (int i = 0; i < m; i++)
+    {
+        std::cout << "line " << i + 1 << " of matrix ";
+        for (int j = 0; j < n; ++j)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    
     for (int i = 0; i < m; i++)
       {
         delete[] arr[i];
@@ -239,4 +307,122 @@ void count_sort(int** arr, int m, int n, bool(*comp)(int, int))
         std::cout << final_line[j] << " ";
     }
     std::cout << "\n";
+}
+
+
+void insert_sort(int** arr, int m, int n, bool(*comp)(int, int))
+{
+    int x;
+long i, j;
+for (i = 1; i < n; i++)
+ { 
+    x = arr[m][i];
+        for (j = i - 1; j >= 0 && comp(arr[m][j], x); j--)
+        {
+            arr[m][j + 1] = arr[m][j];
+            arr[m][j] = x; 
+        }
+ }
+ for (i = 0; i < n; i++)
+ {
+    std::cout << arr[m][i] << " ";
+ }
+    std::cout << "\n";
+}
+
+void shell_sort(int** arr, int m, int n, bool(*comp)(int, int))
+{
+    int h, i, t;
+int k; 
+h = n / 2; 
+while (h > 0)
+{
+ 
+ k = 1;
+ while (k)
+ {
+    
+    k = 0;
+    for (i = 0; i < n - h; i++)
+    {
+    
+        if (comp(arr[m][i], arr[m][i + h]))
+        {
+                t = arr[m][i];
+                arr[m][i] = arr[m][i + h];
+                arr[m][i + h] = t;
+                k = 1; 
+        }
+    }
+ }
+ h = h / 2; 
+ }
+ for (i = 0; i < n; i++)
+ {
+    std::cout << arr[m][i] << " ";
+ }
+    std::cout << "\n";
+}
+
+
+
+void tmp(int**& temp, int m, int n)
+{
+    temp = new int* [m];
+    for (int i = 0; i < m; i++)
+    {
+        temp[i] = new int[n];
+    }
+}
+
+void merge(int **arr,int m, int left, int mid, int right, int** temp, bool(*comp)(int, int))
+{
+    int i = left;
+    int j = mid;
+    int k = left;
+
+    while(i < mid && j < right)
+    {
+        if(comp(arr[m][i], arr[m][j]))
+        {
+            temp[m][k] = arr[m][i];
+            i++;
+        }
+        else
+        {
+            temp[m][k] = arr[m][j];
+            j++;
+        }
+        k++;
+    }
+    while(i < mid)
+    {
+        temp[m][k] = arr[m][i];
+        i++;
+        k++;
+    }
+    while(j < right)
+    {
+        temp[m][k] = arr[m][j];
+        j++;
+        k++;
+    }
+
+    for(int t = left; t < right; t++)
+      {  arr[m][t] = temp[m][t]; }
+
+    
+
+}
+
+void mergeSort(int** arr, int m, int left, int right, int** temp, bool(*comp)(int, int))
+{
+    if(right - left <= 1) return;
+
+    int mid = (left + right) / 2;
+
+    mergeSort(arr, m, left, mid, temp, comp);
+    mergeSort(arr, m, mid, right, temp, comp);
+
+    merge(arr, m, left, mid, right, temp, comp);
 }
