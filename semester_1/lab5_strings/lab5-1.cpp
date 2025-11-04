5e9239e388bf86f25ee896ba695604108c403305
maximantonovich <antonovich.maxim@gmail.com>
2025-11-04 11:09:02 +0300

    added a template for lab 5

---
+++ semester_1/lab5_strings/lab5-1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+std::string write_str();
+char divider(std::string str);
+ biggest_word(std::string str, char separator);
+
+
+
+int main()
+{
+	std::string str = write_str();
+	
+	int length = str.size();
+
+
+	biggest_word(str, separator);
+
+	char separator = divider(str);
+
+}
+
+std::string write_str()
+{
+	std::string str;
+	getline(std::cin, str);
+	return str;
+}
+
+char divider(std::string* str)
+{
+	std::cout << "write what do you want divider to be?";
+	std::cin.ignore();
+	int divider;
+	std::cin >> divider;
+	return divider;
+ }
+
+
