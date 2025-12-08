5898f0f71f6ebe26b4b0845af30e60cd898c7a3c
maximantonovich <antonovich.maxim@gmail.com>
2025-12-09 00:12:07 +0300

    added with cpp file

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,40 @@
 #pragma once
+#include <initializer_list>
+#include <cstdint>
+#include <iostream>
 
 class Vector {
-    // your code goes here
+private:
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+    int* arr_ = nullptr;
+
+public:
+    Vector();
+    Vector(size_t);
+    Vector(std::initializer_list<int>);
+
+    Vector(const Vector&);
+    ~Vector();
+
+    Vector& operator=(const Vector&);
+
+    size_t get_size() const;
+    size_t get_capacity() const;
+
+    void Swap(Vector&);
+
+    int& operator[](size_t) const;
+
+    int& operator[](size_t);
+    int& At(size_t) const;
+    int& At(size_t);
+    size_t Size() const;
+    size_t Capacity() const;
+    void PushBack(int);
+    void reallocate(size_t);
+    void PopBack();
+    void Clear();
+    void Reserve(size_t);
 };
+std::ostream& operator<<(std::ostream&, const Vector&);
