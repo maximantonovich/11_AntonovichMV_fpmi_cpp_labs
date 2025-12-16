7da175c32210bd15a7a3f3d03f43cc64b1a25e8b
maximantonovich <antonovich.maxim@gmail.com>
2025-12-16 09:23:04 +0300

    fixed issue with first word being the biggest

--- semester_1/lab6_files/lab6-1.cpp
+++ semester_1/lab6_files/lab6-1.cpp
@@ -3,39 +3,39 @@
 #include <fstream>
 #include <vector>
 
-void read_words_from_file(const std::string& filename, std::vector<std::string>& words,std::vector<size_t>& amount);
+void read_words_from_file(const std::string& filename, std::vector<std::string>& words, std::vector<size_t>& amount);
 std::string most_times_repeat_word(std::vector<std::string>& words, std::vector<size_t>& amount);
 
 int main()
 {
-	try
+    try
     {
-        const std::string input {"input.txt"};
+        const std::string input{ "input.txt" };
 
         std::vector<std::string> words;
         std::vector<size_t> amount;
 
         read_words_from_file(input, words, amount);
-        
+
         std::string THE_word;
         THE_word = most_times_repeat_word(words, amount);
 
-        std::cout << "\n"<< "word repeated most times is: " <<  THE_word;
+        std::cout << "\n" << "word repeated most times is: " << THE_word;
 
 
     }
-    catch(const char* msg)
+    catch (const char* msg)
     {
         std::cerr << msg << '\n';
     }
-    
-	
-	
+
+
+
 }
 
 bool same_word(std::string word, std::vector<std::string>& words, size_t num)
 {
-    if(word.size() == words[num].size())
+    if (word.size() == words[num].size())
     {
         size_t length;
         if (word.size() > words[num].size())
@@ -46,14 +46,14 @@ bool same_word(std::string word, std::vector<std::string>& words, size_t num)
         {
             length = words[num].size();
         }
-        
+
         for (size_t i = 0; i < length; i++)
         {
             if (word[i] != words[num][i])
             {
                 return 0;
             }
-            
+
         }
     }
     else
@@ -63,29 +63,29 @@ bool same_word(std::string word, std::vector<std::string>& words, size_t num)
     return 1;
 }
 
-void read_words_from_file(const std::string& filename, std::vector<std::string>& words,std::vector<size_t>& amount)
+void read_words_from_file(const std::string& filename, std::vector<std::string>& words, std::vector<size_t>& amount)
 {
     words.clear();
     amount.clear();
-    std::ifstream in {filename};
+    std::ifstream in{ filename };
 
 
     if (!in.is_open())
     {
-       throw ("Couldnt open file");
+        throw ("Couldnt open file");
     }
 
     if (in.peek() == EOF)
     {
         throw ("File is empty");
     }
-    
+
     std::string word;
 
     in >> word;
     words.push_back(word);
     amount.push_back(1);
-    
+
     while (in >> word)
     {
         bool found_same;
@@ -94,37 +94,37 @@ void read_words_from_file(const std::string& filename, std::vector<std::string>&
             found_same = false;
             if (same_word(word, words, i))
             {
-             found_same = true;
-             amount.at(i)++;
-             break;
+                found_same = true;
+                amount.at(i)++;
+                break;
             }
-            
+
         }
-        
+
         if (!found_same)
         {
             words.push_back(word);
             amount.push_back(1);
-        } 
+        }
 
-        
+
     }
     std::cout << "words and times they are used: ";
     for (size_t j = 0; j < words.size(); j++)
     {
-       std::cout << words[j] << "(";
+        std::cout << words[j] << "(";
         std::cout << amount[j] << ") ";
     }
-    std::cout <<std::endl;
-    
-    
+    std::cout << std::endl;
+
+
 }
 
 std::string most_times_repeat_word(std::vector<std::string>& words, std::vector<size_t>& amount)
 {
     std::string repeat_word;
     size_t max = amount[0];
-    size_t max_index;
+    size_t max_index = -1;
     for (size_t i = 1; i < amount.size(); i++)
     {
         if (max < amount[i])
@@ -136,7 +136,11 @@ std::string most_times_repeat_word(std::vector<std::string>& words, std::vector<
         {
             throw "there are mor than 1 word that was used the same amount of times";
         }
-        
+
+    }
+    if (max_index == -1)
+    {
+        return words[0];
     }
     repeat_word = words[max_index];
     return repeat_word;
