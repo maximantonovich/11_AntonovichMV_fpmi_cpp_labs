bcd3bd0269ba595fad1fbb06f4c1c51a48972bc9
maximantonovich <antonovich.maxim@gmail.com>
2025-12-17 20:50:37 +0300

    passed all tests

--- semester_1/rgr1_forward_list/forward_list/forward_list_impl.cpp
+++ semester_1/rgr1_forward_list/forward_list/forward_list_impl.cpp
@@ -1,12 +1,13 @@
 #include "forward_list_impl.h"
-ForwardList::ForwardList() {
+ForwardList::ForwardList() : head(nullptr), size_(0) {
 }
 
-ForwardList::ForwardList(const ForwardList& rhs) {
+ForwardList::ForwardList(const ForwardList& rhs) : head(nullptr), size_(0) {
     if (rhs.head != nullptr) {
         head = new Node(rhs.head->value_);
         Node* current = head;
         Node* rhs_current = rhs.head->next_;
+        size_ = rhs.size_;
 
         while (rhs_current != nullptr) {
             current->next_ = new Node(rhs_current->value_);
@@ -17,24 +18,19 @@ ForwardList::ForwardList(const ForwardList& rhs) {
 }
 
 ForwardList::ForwardList(size_t count, int32_t value) {
-
+    head = nullptr;
+    size_ = 0;
     for (size_t i = 0; i < count; i++) {
         PushFront(value);
     }
 }
 
-ForwardList::ForwardList(std::initializer_list<int32_t> init) {
-    head = nullptr;
+ForwardList::ForwardList(std::initializer_list<int32_t> init) : head(nullptr), size_(0) {
+
     for (std::initializer_list<int32_t>::iterator i = init.end(); i != init.begin();) {
         i--;
         PushFront(*i);
     }
-}
-
-void ForwardList::PushFront(int32_t value) {
-    Node* new_el = new Node(value);
-    new_el->next_ = head;
-    head = new_el;
 }
 
 ForwardList& ForwardList::operator=(const ForwardList& rhs) {
@@ -43,6 +39,7 @@ ForwardList& ForwardList::operator=(const ForwardList& rhs) {
     }
     ForwardList temp(rhs);
     std::swap(head, temp.head);
+    size_ = temp.size_;
     return *this;
 }
 
@@ -52,4 +49,95 @@ ForwardList::~ForwardList() {
         head = head->next_;
         delete temp;
     }
+}
+
+void ForwardList::PushFront(int32_t value) {
+    Node* new_el = new Node(value);
+    new_el->next_ = head;
+    head = new_el;
+    size_++;
+}
+
+void ForwardList::PopFront() {
+    if (head != nullptr) {
+        Node* next = head->next_;
+        delete head;
+        head = next;
+        size_--;
+    }
+}
+
+void ForwardList::Remove(int32_t value) {
+    if (head != nullptr) {
+        Node* prev;
+        Node* cur;
+
+        while (head != nullptr && head->value_ == value) {
+            PopFront();
+        }
+
+        if (head == nullptr) {
+            return;
+        }
+
+        prev = head;
+        cur = head->next_;
+
+        while (cur != nullptr) {
+            if (cur->value_ == value) {
+                Node* temp = cur->next_;
+                delete cur;
+                cur = temp;
+                prev->next_ = cur;
+                size_--;
+            } else {
+                prev = cur;
+                cur = cur->next_;
+            }
+        }
+    }
+}
+
+size_t ForwardList::Size() const {
+    return size_;
+}
+
+void ForwardList::Clear() {
+    while (head != nullptr) {
+        Node* temp = head;
+        head = head->next_;
+        delete temp;
+    }
+    size_ = 0;
+}
+
+bool ForwardList::FindByValue(int32_t value) {
+    Node* cur = head;
+    while (cur != nullptr) {
+        if (cur->value_ == value) {
+            return 1;
+        }
+        cur = cur->next_;
+    }
+    return false;
+}
+
+void ForwardList::Print(std::ostream& out) {
+    if (head != nullptr) {
+        Node* cur = head;
+        out << cur->value_;
+        cur = head->next_;
+        while (cur != nullptr) {
+            out << " ";
+            out << cur->value_;
+            cur = cur->next_;
+        }
+    }
+}
+
+int32_t ForwardList::Front() const {
+    if (head != nullptr) {
+        return head->value_;
+    }
+    throw "Front is empry";
 }
